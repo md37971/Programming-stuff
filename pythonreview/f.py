a0dc40b81c8b4e7911aec36bc969ed959a2183ad
md37971 <111905085+md37971@users.noreply.github.com>
2022-09-01 18:06:40 -0500

    Add files via upload

---
+++ pythonreview/f.py
@@ -0,0 +1,14 @@
+#----------------------------------------------------------------------------
+      # Created By  : Michael Delgado       
+      # Created Date: 9/1/2022      
+      # Project Name: Task F    
+      # Project Purpose: To count to 100
+      # Project Function: To count to 100
+      # ---------------------------------------------------------------------------
+      # Psuedocode:
+      # Create a for loop with the variable "i"
+      # prints the number based on the variable
+      # ---------------------------------------------------------------------------
+
+for i in range(1,101):
+      print(i)
---
+++ pythonreview/g.py
@@ -0,0 +1,14 @@
+#----------------------------------------------------------------------------
+      # Created By  : Michael Delgado      
+      # Created Date: 9/1/2022      
+      # Project Name: Task G     
+      # Project Purpose: Count to 100 with less code    
+      # Project Function: To count to 100 in a few lines of code   
+      # ---------------------------------------------------------------------------
+      # Psuedocode:
+      # create a for loop with the variable "i"
+      # outputs the counting i variable
+      # ---------------------------------------------------------------------------
+
+for i in range(1,101):
+    print(i)
