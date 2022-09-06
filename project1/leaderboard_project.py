3339319f25961c447b2e485f7ab82cc5a8937d9c
md37971 <111905085+md37971@users.noreply.github.com>
2022-09-06 12:38:55 -0500

    Add files via upload

---
+++ project1/leaderboard_project.py
@@ -0,0 +1,12 @@
+      #----------------------------------------------------------------------------
+      # Created By  : Michael Delgado       
+      # Created Date: 9/6/22      
+      # Project Name: Leaderboard For a Video Game
+      # Project Purpose: Leaderboard   
+      # Project Function: User will input their leaderboard stats by typing their info, which will input their leaderboard stats.  
+      # ---------------------------------------------------------------------------
+      # Psuedocode:
+      # 
+      # ---------------------------------------------------------------------------
+
+
