84ab4e9621906000d91c065f1f88ab15ff46599d
md37971 <111905085+md37971@users.noreply.github.com>
2022-09-21 12:57:29 -0500

    Update screengui.py

--- codealong/screengui.py
+++ codealong/screengui.py
@@ -1,4 +1,5 @@
 from ast import While
+from http.client import CONTINUE
 import turtle
 import random
 import math
@@ -9,21 +10,38 @@ peepoo = turtle.Turtle()
 
 poop.colormode(255)
 while True:
+    peepoo.penup()
+    peepoo.goto(random.randint(1,30),random.randint(1,30))
+    peepoo.pendown()
     r = random.randint(0,255)
     g = random.randint(0,255)
     b = random.randint(0,255)
-
     peepoo.color((r,g,b))
-
     cat = 0
     sides = int(poop.numinput("sides","How many sides do you want? "))
     angle = 360 / sides
     distance = 360/(sides/2)
     peepoo.begin_fill()
 
+    #background color
+    colorquestion = poop.textinput("Background","Do you want a random color background or specific?")
+    if colorquestion == "SPECIFIC".lower():
+        colorinput = poop.textinput("Color","What color do you want?")
+    else:
+        colorinput = "#" + str(random.randint(10000,99999))
+
+    poop.bgcolor(colorinput)
+
     while cat < sides:
         peepoo.forward(distance)
         peepoo.right(angle)
         cat += 1
 
-turtle.Screen().exitonclick()
+    #end
+    answer = poop.textinput("Dialogue", "Do you want to quit or retry? Type 'Quit' or 'Retry'")
+    if answer == "QUIT".lower():
+        exit()
+    elif answer == "RETRY".lower():
+        continue
+    else:
+        exit()
