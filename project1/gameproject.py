e4c8a95ad29f0ec83dd0bc7b8cf03f7f9bfc6c22
md37971 <111905085+md37971@users.noreply.github.com>
2022-11-09 12:17:29 -0600

    Update and rename leaderboard_project.py to gameproject.py

--- project1/gameproject.py
+++ project1/gameproject.py
@@ -1,9 +1,9 @@
       #----------------------------------------------------------------------------
       # Created By  : Michael Delgado       
       # Created Date: 9/6/22      
-      # Project Name: Leaderboard For a Video Game
-      # Project Purpose: Leaderboard   
-      # Project Function: User will input their leaderboard stats by typing their info, which will input their leaderboard stats.  
+      # Project Name: Untitled Video Game Project
+      # Project Purpose: Game Project   
+      # Project Function:
       # ---------------------------------------------------------------------------
       # Psuedocode:
       # 
